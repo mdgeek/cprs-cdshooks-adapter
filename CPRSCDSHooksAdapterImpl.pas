fa64a0bcb6119a4d085d3f7e228dfa2eb047da76
Douglas Martin <mdgeek1@gmail.com>
2022-10-14 11:33:27 -0400

    Updates

--- CPRSCDSHooksAdapterImpl.pas
+++ CPRSCDSHooksAdapterImpl.pas
@@ -14,12 +14,9 @@ type
     procedure Initialize; override;
     destructor Destroy; override;
   end;
-
 implementation
-
 uses ComServ, Windows, ShellAPI;
-
-function getSystemParams(broker: ICPRSBroker): TStringList;
+function getSystemParams(broker: ICPRSBroker; state: ICPRSState): TStringList;
 var
   rpcResult: String;
 begin
@@ -27,9 +24,11 @@ begin
   Result.Delimiter := #10;
   broker.SetContext('CDSP');
   broker.ClearParameters;
+  broker.ClearResults;
+  broker.Param[0] := state.PatientDFN;
+  broker.ParamType[0] := bptLiteral;
   broker.CallRPC('CDSP GETPARAMS');
   rpcResult := broker.Results;
-  rpcResult := StringReplace(rpcResult, 'CDSP ', '', [rfReplaceAll]);
   rpcResult := LowerCase(rpcResult);
   rpcResult := StringReplace(rpcResult, ' ', '_', [rfReplaceAll]);
   Result.Text := rpcResult;
@@ -41,7 +40,6 @@ begin
   if second.StartsWith('/') then second := second.Substring(2);
   Result := first + second;
 end;
-
 function appendQueryString(path: String; qs: String): String;
 begin
   if path.Contains('?') then Result := path + '&' + qs
@@ -60,17 +58,14 @@ begin
     Result := 0;
   end;
 end;
-
 procedure TCPRSCDSHooksAdapterCoClass.Initialize;
 begin
   inherited;
 end;
-
 destructor TCPRSCDSHooksAdapterCoClass.Destroy;
 begin
   inherited;
 end;
-
 function TCPRSCDSHooksAdapterCoClass.Execute(const CPRSBroker: ICPRSBroker; const CPRSState: ICPRSState;
           const Param1, Param2, Param3: WideString; var Data1,
           Data2: WideString): WordBool;
@@ -81,6 +76,7 @@ var
   url: String;
   httpClient: TIdHTTP;
   endPoint: String;
+  qs: String;
 begin
   try
     data := TStringList.Create;
@@ -104,24 +100,23 @@ begin
     data.AddPair('patientDob', CPRSState.PatientDOB);
     data.AddPair('locationId', IntToStr(CPRSState.LocationIEN));
     data.AddPair('locationName', CPRSState.LocationName);
-    params := getSystemParams(CPRSBroker);
+    params := getSystemParams(CPRSBroker, CPRSState);
     data.addStrings(params);
     endpoint := data.Values['cdshook_proxy_endpoint'];
     ShowMessage(data.Text);
     httpClient := TIdHTTP.Create;
     httpClient.Post(pathJoin(endpoint, 'forward'), data);
-    url := appendQueryString(data.Values['cdshook_client_endpoint'], 'handle=' + CPRSState.Handle);
+    qs := Format('handle=%s&proxy=%s', [CPRSState.Handle, data.values['cdshook_proxy_endpoint']]);
+    url := appendQueryString(data.Values['cdshook_client_endpoint'], qs);
     ShellExecute(ToHWND(CPRSState.Handle), 'open', PWideChar(url), nil, nil, SW_SHOWNORMAL);
   except
     On e:Exception do begin
       ShowMessage('Error: ' + e.Message);
     end;
   end;
-
   FreeAndNil(data);
   FreeAndNil(params);
 end;
-
 initialization
   TAutoObjectFactory.Create(ComServer, TCPRSCDSHooksAdapterCoClass, Class_CPRSCDSHooksAdapterCoClass,
     ciMultiInstance, tmApartment);
